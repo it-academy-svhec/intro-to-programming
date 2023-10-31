c22c69eac4b1291c8e1d8fb4395657b52cbc3145
jcook135 <jacobcook@svhec.email>
2023-10-31 16:01:32 -0400

    Add example for ignoring returns

---
+++ examples/python_ignoring_return_values.py
@@ -0,0 +1,14 @@
+def create_user_account(username, password="820BruceStreet"):
+    print(f'Created user {username} with password "{password}"')
+    return [username, password]
+
+
+username = input("Enter a username: ")
+
+new_user = create_user_account(username)
+print(new_user[0], new_user[1])
+
+
+username = input("Enter a username: ")
+
+create_user_account(username)
