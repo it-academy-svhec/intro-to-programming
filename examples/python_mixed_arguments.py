06b7ce3fbc592e5669819f61adb6b385ac1c5be6
jcook135 <jacobcook@svhec.email>
2023-10-31 13:38:24 -0400

    Add exmaple for mixed args

---
+++ examples/python_mixed_arguments.py
@@ -0,0 +1,5 @@
+def printReceipt(order_number, subtotal, date):
+    print(f"Order #{order_number}\nSubtotal: ${subtotal}\nPlaced on {date}")
+
+
+printReceipt(order_number=235, 60.50, date="10-31-23")
