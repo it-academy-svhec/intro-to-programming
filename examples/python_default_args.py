ad8c325e2a23b3fc65469ad16b116a4638f8fc0d
jcook135 <jacobcook@svhec.email>
2023-10-31 14:37:13 -0400

    Add default args example

---
+++ examples/python_default_args.py
@@ -0,0 +1,11 @@
+def create_user_account(username, password="820BruceStreet"):
+    print(f"Created user {username} with password "{password}")
+
+
+username = input("Enter a username: ")
+password = input("Enter a password: ")
+
+if not password:
+    create_user_account(username)
+else:
+    create_user_account(username, password)
