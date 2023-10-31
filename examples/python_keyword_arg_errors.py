8f23ce366ecfa5a6a3d03932b161ebccc92c745f
jcook135 <jacobcook@svhec.email>
2023-10-31 13:30:35 -0400

    Add example for Python keyword errors

---
+++ examples/python_keyword_arg_errors.py
@@ -0,0 +1,5 @@
+def printReceipt(order_number, subtotal, date):
+    print(f"Order #{order_number}\nSubtotal: ${subtotal}\nPlaced on {date}")
+
+
+printReceipt(number=235, subtotal=60.50, date="10-31-23")
