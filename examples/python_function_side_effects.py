df36f8110190e54fbfa981a205ccd79d78337415
jcook135 <jacobcook@svhec.email>
2023-11-02 14:04:06 -0400

    Add side effects

---
+++ examples/python_function_side_effects.py
@@ -0,0 +1,36 @@
+VA_SALES_TAX = 0.053
+
+
+def calculate_tax(price, rate):
+    return price * rate
+
+
+def create_items(items):
+    item_count = int(input("How many items are you purchasing?"))
+
+    for item in range(item_count):
+        name = input("Name: ")
+        price = float(input("Price: "))
+
+        new_item = [name, price]
+
+        items.append(new_item)
+
+
+def print_items(items):
+    for item in items:
+        item[1] += calculate_tax(item[1], VA_SALES_TAX)
+        print(f"Name: {item[0]} | Price: {item[1]}")
+
+
+price = 100
+
+subtotal = price + calculate_tax(price, VA_SALES_TAX)
+
+items = []
+
+create_items(items)
+print_items(items)
+
+create_items(items)
+print_items(items)
