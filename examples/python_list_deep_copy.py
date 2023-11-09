46e81ab8ffa872bccb774b0f8b72e9f286705a05
jcook135 <jacobcook@svhec.email>
2023-11-09 18:11:33 -0500

    Update comment for deep copy

--- examples/python_list_deep_copy.py
+++ examples/python_list_deep_copy.py
@@ -1,12 +1,14 @@
+import copy
+
 def subtotal_items_with_discount(items, discount):
     # Slice copy of the list
-    copy_items = items[:]
+    copy_items = copy.deepcopy(items)
     subtotal = 0.0
 
     for item in copy_items:
         item[1] *= 1 - discount
         subtotal += item[1]
-        print(f"Name: ${item[0]} | Price: ${item[1]}")
+        print(f"Name: {item[0]} | Price: ${item[1]}")
 
     print(f"Subtotal: ${subtotal}")
 
@@ -17,7 +19,7 @@ BLACK_FRIDAY_RATE = 0.10
 
 subtotal_items_with_discount(items, BLACK_FRIDAY_RATE)
 
-# Ensure data was mutated
+# Ensure data was not mutated
 assert items[0][0] == "Guitar"
 assert items[0][1] == 500
 
